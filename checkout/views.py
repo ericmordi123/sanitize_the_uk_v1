8adc8563af474f3f49f7b3a27eded140f54544a2
Eric Mordi <ericmordi@gmail.com>
2020-07-29 20:26:38 +0000

    added stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HALZ7IoqgGqaKpNXr15BRgcMTcGGCPWGkMHWVUAoFRAbqsdocoS3t35aOVEyt9nAiEJWgFLhSi5TcCWEVezYTod00pkWdYALY',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
